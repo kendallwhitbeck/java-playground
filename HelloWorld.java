cdd0b965ac1a3dc3b3b0f3a15425ea7c8a9190f1
Kendall Whitbeck <kendallwhitbeck@gmail.com>
2024-04-18 10:59:42 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+// print(f"Hello world! {arg1}") # Pythonic representation
+
+public class HelloWorld{
+
+    public static void main(String[] args){ 
+
+        System.out.println("Hello World!" + " " + args[0] + " " + args[1]);
+        System.out.println(String.format("Hello World! %s %s", args[0],args[1]));
+    }
+}
