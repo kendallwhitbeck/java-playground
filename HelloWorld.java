d2db827fc8666deb214444765bf4ad845be76953
Kendall Whitbeck <kendallwhitbeck@gmail.com>
2024-04-18 15:36:04 -0700

    HelloWorld Compile & Usage Comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,14 @@
+// Compile: javac HelloWorld.java
+// Usage: java HelloWorld arg1 arg2
+
 // print(f"Hello world! {arg1}") # Pythonic representation
 
 public class HelloWorld{
 
-    public static void main(String[] args){ 
+    public static void main(String[] args){
 
         System.out.println("Hello World!" + " " + args[0] + " " + args[1]);
         System.out.println(String.format("Hello World! %s %s", args[0],args[1]));
     }
 }
+
